ab20ac1d5cdfd5695d0575cfdf44483153bf543d
Victor Azevedo <victor.azevedo@mail.com>
2023-12-06 08:52:10 +0100

    Sort by popularity, immutable cache

--- src/apis/fonts.ts
+++ src/apis/fonts.ts
@@ -14,22 +14,32 @@ type Fontsource = {
 type FontList = Pick<Fontsource, 'family' | 'subsets' | 'weights' | 'variable'>[]
 
 export default async function fonts(headers: Headers): Promise<Response> {
-	const fontlist: FontList = []
-
-	headers.set('Cache-Control', 'public, max-age=86400, stale-while-revalidate=604800')
+	headers.set('Cache-Control', 'public, max-age=604800, immutable')
 	headers.set('Content-Type', 'application/json')
 
-	try {
-		const resp = await fetch('https://api.fontsource.org/v1/fonts')
-		const fonts = (await resp.json()) as Fontsource[]
+	const fontlist: FontList = []
 
-		for (const item of fonts) {
-			if (item.subsets.includes('latin') && item.category !== 'icons') {
+	try {
+		const responses = await Promise.all([
+			fetch('https://api.fontsource.org/v1/fonts'),
+			fetch('https://cdn.jsdelivr.net/gh/victrme/bonjourr-apis@fonts/src/assets/font_popularity.txt'),
+		])
+
+		const fonts = (await responses[0]?.json()) as Fontsource[]
+		const popularity = (await responses[1]?.text()).split(',')
+		const familyOnly = fonts.map((font) => font.family)
+
+		let font: Fontsource | undefined
+
+		for (const family of popularity) {
+			font = fonts[familyOnly.indexOf(family)]
+
+			if (font && font.subsets.includes('latin') && font.category !== 'icons' && font.category !== 'other') {
 				fontlist.push({
-					family: item.family,
-					subsets: item.subsets,
-					weights: item.weights,
-					variable: item.variable,
+					family: font.family,
+					subsets: font.subsets,
+					weights: font.weights,
+					variable: font.variable,
 				})
 			}
 		}
