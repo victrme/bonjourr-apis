a06a53656b6fcad7fbf422960da5401b552326ca
Victor Azevedo <victor.azevedo@mail.com>
2024-04-01 15:26:33 +0200

    Removed test coverage and vite dependency

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,10 +3,6 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
 	test: {
 		watch: false,
-		include: ['src/index.test.ts'],
-		coverage: {
-			provider: 'v8',
-			enabled: false,
-		},
+		include: ['src/*.test.ts'],
 	},
 })
