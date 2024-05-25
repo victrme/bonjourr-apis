ce7c71b864e2ebd0eb08f694f74e63c4a968c58a
Victor Azevedo <victor.azevedo@mail.com>
2024-05-25 12:09:17 +0200

    Transform icon id from accuweather to openweathermap

--- src/apis/weather/accuweather.ts
+++ src/apis/weather/accuweather.ts
@@ -40,6 +40,14 @@ export async function accuweather(
 	const response = (await scraper.fetch(request)) as Worker.Response
 	const json = await response.json<AccuWeather.Data>()
 
+	// Transform id from Accuweather to OpenWeatherMap
+	let owmIconId = 801
+	for (const { accu, owm } of Object.values(weatherConditions)) {
+		if (accu.includes(json.now.icon)) {
+			owmIconId = owm
+		}
+	}
+
 	const result: WeatherResponse = {
 		city: city ? city : undefined,
 		ccode: ccode ? ccode : undefined,
@@ -53,8 +61,8 @@ export async function accuweather(
 			sunset: json.sun.set / 1000,
 			weather: [
 				{
-					id: json.now.icon,
-					icon: json.now.icon + '',
+					id: owmIconId,
+					icon: owmIconId.toString(),
 					main: json.now.description,
 					description: json.now.description,
 				},
@@ -67,4 +75,51 @@ export async function accuweather(
 	}
 
 	return result
+}
+
+const weatherConditions = {
+	clearsky: {
+		accu: [1, 2, 33, 34],
+		owm: 800,
+	},
+	fewclouds: {
+		accu: [3, 4, 5, 30, 31, 35, 36, 37],
+		owm: 801,
+	},
+	brokenclouds: {
+		accu: [6, 7],
+		owm: 802,
+	},
+	overcastclouds: {
+		accu: [8, 38],
+		owm: 803,
+	},
+	lightrain: {
+		accu: [14, 26, 29, 39],
+		owm: 500,
+	},
+	lightdrizzle: {
+		accu: [39],
+		owm: 300,
+	},
+	showerdrizzle: {
+		accu: [13],
+		owm: 312,
+	},
+	showerrain: {
+		accu: [12, 33, 34],
+		owm: 504,
+	},
+	thunderstorm: {
+		accu: [15, 16, 17, 41, 42],
+		owm: 200,
+	},
+	snow: {
+		accu: [19, 20, 21, 22, 23, 24, 25, 43, 44],
+		owm: 600,
+	},
+	mist: {
+		accu: [11],
+		owm: 701,
+	},
 }
