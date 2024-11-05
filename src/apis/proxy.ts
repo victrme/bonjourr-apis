2230d259a041d67825d2b98118f9ae4acc26eef0
Victor Azevedo <victor.azevedo@mail.com>
2024-11-05 15:06:26 +0100

    CF Response type in proxy

--- src/apis/proxy.ts
+++ src/apis/proxy.ts
@@ -1,10 +1,17 @@
-export default async function proxy(req: Request): Promise<ResponseInit> {
+export default async function proxy(req: Request): Promise<Response> {
 	const reqUrl = new URL(req.url)
 	const query = reqUrl.searchParams.get('query') ?? ''
 
 	try {
 		const queryUrl = new URL(query)
-		return await fetch(queryUrl)
+		const resp = await fetch(queryUrl)
+		const text = await resp.text()
+		return new Response(text, {
+			headers: {
+				'content-type': 'text/plain',
+				'cache-control': 'max-age=10, immutable',
+			},
+		})
 	} catch (_) {
 		return new Response(undefined, {
 			status: 500,
