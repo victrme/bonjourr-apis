cd883a14b1ebcccd557fbe1a88997d0fc173ddff
Victor Azevedo <victor.azevedo@mail.com>
2025-02-10 06:45:29 +0100

    Use correct ids for pixabay collections

--- assets/pixabay_id_extractor.js
+++ assets/pixabay_id_extractor.js
@@ -2,16 +2,19 @@
 // Scrolle bien tout en bas pour charger toute les images
 // Colle ça dans la console, récupère les ids
 
+// Images
+
 console.log(
-	Object.values(document.querySelectorAll('div > a[class] > img'))
-		.map((image) => image.src)
-		.map((src) => src.slice(src.lastIndexOf('/') + 1, src.indexOf('-')).split('_')[0])
+	Object.values(document.querySelectorAll('div > a[data-id]'))
+		.map((a) => a.dataset.id)
 		.join(', ')
 )
+
+// Videos
 
 console.log(
 	Object.values(document.querySelectorAll('div > a[class] > img'))
 		.map((image) => image.src)
-		.map((src) => src.slice(src.lastIndexOf('-') + 1, src.indexOf('_', src.lastIndexOf('-'))))
+		.map((src) => src.slice(src.lastIndexOf('/') + 1, src.indexOf('-')).split('_')[0])
 		.join(', ')
 )
