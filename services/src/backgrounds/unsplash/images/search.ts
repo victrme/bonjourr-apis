8eea36037cfcf6af70a960a924bbd9bb9ceb0af9
Tahoe <50768582+morceaudebois@users.noreply.github.com>
2026-07-19 12:55:58 +0200

    Frame mode compat for unsplash search + collections

--- services/src/backgrounds/unsplash/images/search.ts
+++ services/src/backgrounds/unsplash/images/search.ts
@@ -5,12 +5,19 @@ import type { UnsplashImage } from '../types.ts'
 import type { Image } from '../../types.ts'
 
 export async function unsplashImagesSearch(url: URL, headers: Headers): Promise<Response> {
-	const orientation = url.searchParams.get('orientation') ?? 'landscape'
+	// this whole thing probably should be reworked with URLSearchParams() but my bravery has limits
+	const orientationFromUrl = url.searchParams.get('orientation')
+
+	let orientation = ''
+	if (orientationFromUrl !== "any") {
+		orientation = '&orientation=' + (orientationFromUrl ?? 'landscape')
+	}
+
 	const query = url.searchParams.get('query') ?? ''
 	const w = url.searchParams.get('w') ?? '1920'
 	const h = url.searchParams.get('h') ?? '1080'
 
-	const searchQuery = `?query=${query}&orientation=${orientation}&content_filter=high&count=20`
+	const searchQuery = `?query=${query}${orientation}&content_filter=high&count=20`
 	const images: UnsplashImage[] = await fetchRandomUnsplash(searchQuery)
 	const result: Image[] = []
 
